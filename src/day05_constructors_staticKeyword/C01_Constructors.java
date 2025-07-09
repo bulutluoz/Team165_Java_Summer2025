12ea79c3be4a43ee496da769000bb15b98c13cce
bulutluoz <abbulutluoz@gmail.com>
2025-07-09 19:50:07 +0200

    5.ders sonu

---
+++ src/day05_constructors_staticKeyword/C01_Constructors.java
@@ -0,0 +1,33 @@
+package day05_constructors_staticKeyword;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class C01_Constructors {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        List<String> isimler = new ArrayList<>();
+        String str = new String();
+        Random rnd = new Random();
+
+        /*
+            ister biz olusturmus olalim, isterse de java olustursun
+            Bir class'daki hazir method'lar ve varoalan variable'lara
+            ulasabilmek icin farkli yontemler vardir.
+
+            bu yontemlerden birincisi
+            ozelliklerini kullanmak istediginiz class'dan bir obje olusturmaktir.
+
+            Bizim olusturdugumuz bir class'daki ozellikleri(method+variable)
+            kullanmak icin de o class'dan obje olusturabiliriz
+         */
+
+
+
+
+    }
+}
---
+++ src/day05_constructors_staticKeyword/C02_Car.java
@@ -0,0 +1,13 @@
+package day05_constructors_staticKeyword;
+
+public class C02_Car {
+
+    /*
+        arabam.com gibi 2.el araba satan bir uygulama yapacagiz
+     */
+
+    String marka;
+    String model;
+    int yil;
+    int fiyat;
+}
